32b6698cc64d0e4f4ebabad94569924a72bd1ae8
s777ia <23046128@myrp.edu.sg>
2025-02-01 17:26:56 +0800

    added process.env

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -10,12 +10,12 @@ export const useSignup = () => {
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch('/api/user/signup', {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/user/signup`, {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
-                body: JSON.stringify({ email, password })
-            }
+                'Content-Type': 'application/json',     
+            },
+            body: JSON.stringify({ email, password })
         })
         const json = await response.json()
 
